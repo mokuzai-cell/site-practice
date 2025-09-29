1376d0bb21937cfab94d38764c131d0bea6715b2
mokuzai-cell <7525109@ed.tus.ac.jp>
2025-09-29 16:24:38 +0900

    スタイル調整、allclear実装

--- src/components/atoms/boothCard_filter_pc.vue
+++ src/components/atoms/boothCard_filter_pc.vue
@@ -1,33 +1,45 @@
 <template>
     <div id="bcf_all">
-        <div id="bcf_title">
-
+        <div>
+            <h2 id="bcf_title">絞り込み</h2>
+            <p id="bcf_allclear" @click="allclear">すべてクリア</p>
         </div>
 <!-- 日付------------------------------------ -->
-        <div class="bcf_wrap">
-            <h3>日付<span>クリア</span></h3>
+        <div class="bcf_wrap" name="date">
+            <div>
+                <span class="bcf_wrap_title">日付</span>
+                <span class="bcf_wrap_clear" @click="wrapclear('date')">クリア</span>
+            </div>
+            <hr />
             <div class="bcf_btn_wrap">
-            <label v-for="value in dates" class="checkBtn"><input type="checkbox" @click="changeDate(value[1])"/><span>{{ value[0] }}</span></label>
+                <label v-for="value in dates" class="checkBtn"><input type="checkbox" @click="changeDate(value[1])" :value="value[1]" /><span>{{ value[0] }}</span></label>
             </div>
         </div>
 <!-- 場所------------------------------------ -->
-        <div class="bcf_wrap">
-            <h3>場所</h3>
+        <div class="bcf_wrap" name="place">
+            <div>
+                <span class="bcf_wrap_title">場所</span>
+                <span class="bcf_wrap_clear" @click="wrapclear('place')">クリア</span>
+            </div>
+            <hr />
             <div class="bcf_btn_wrap">
-            <label v-for="value in places" class="checkBtn"><input type="checkbox" @click="changePlace(value[1])"/><span>{{ value[0] }}</span></label>
+                <label v-for="value in places" class="checkBtn"><input type="checkbox" @click="changePlace(value[1])" :value="value[1]" /><span>{{ value[0] }}</span></label>
             </div>
         </div>
 <!-- タグ------------------------------------ -->
-        <div class="bcf_wrap">
-            <h3>タグ</h3>
+        <div class="bcf_wrap" name="tag">
+            <div>
+                <span class="bcf_wrap_title">タグ</span>
+                <span class="bcf_wrap_clear" @click="wrapclear('tag')">クリア</span>
+            </div>
+            <hr />
             <div class="bcf_btn_wrap">
-            <label v-for="value in tags" class="checkBtn"><input type="checkbox" @click="changeTag(value[1])"/><span>{{ value[0] }}</span></label>
+            <label v-for="value in tags" class="checkBtn"><input type="checkbox" @click="changeTag(value[1])" :value="value[1]" /><span>{{ value[0] }}</span></label>
             </div>
         </div>
     </div>
 </template>
 <script lang="ts">
-
 export default {
     props:{
 
@@ -51,34 +63,99 @@ export default {
     },
     methods: {
         allclear(){
-            this.$emit("allclear");
+            //作るのがめんどくさいので「すべてのwrapのクリアを発動させる」ことで解決します
+            let elems=document.getElementsByClassName('bcf_wrap');
+            if(elems.length==0)return;
+            for(let i=0;i<elems.length;i++){
+                this.wrapclear(elems[i].getAttribute("name")+"");
+            }
+            this.$emit("sendAllclear");
         },
-        wrapclear(wrapname:String){
-
+        wrapclear(wrapname:string){
+            console.log("wrapclear:"+wrapname);
+            for(let elem of document.getElementsByName(wrapname)[0].getElementsByClassName("bcf_btn_wrap")[0].querySelectorAll("input")){
+                console.log(elem.checked);
+                if(elem.checked){
+                    this.$emit("sendChange"+wrapname,elem.value);
+                    elem.checked=false;
+                }
+            }
+            this.$emit("sendWrapclear",wrapname);
         },
-        changeDate(date:String){
+        changeDate(date:string){
             console.log(date);
-            this.$emit("sendChangeDate",date);
+            this.$emit("sendChangedate",date);
         },
-        changePlace(place:String){
+        changePlace(place:string){
             console.log(place);
-            this.$emit("sendChangePlace",place);
+            this.$emit("sendChangeplace",place);
         },
-        changeTag(tag:String){
+        changeTag(tag:string){
             console.log(tag);
-            this.$emit("sendChangeTag",tag);
+            this.$emit("sendChangetag",tag);
         },
     },
 }
 </script>
 <style>
+#bcf_all{
+    width:25dvw;
+    max-width:480px;
+    height:90%;
+    position: fixed;
+    top:5%;
+    left:8px;
+    overflow-y: auto;
+    box-shadow: 4px 4px #182b66;
+    border-top:1px;
+    border-left:1px;
+    border-radius: 8px;
+    border-style: solid;
+    border-color: #182b66;
+    font-family: sans-serif;
+    >*{
+        margin-left:16px;
+        margin-right:16px;
+    }
+}
+#bcf_title{
+    margin-top:8px;
+    margin-bottom:8px;
+}
+#bcf_allclear{
+    text-align:right;
+    font-size:16px;
+    margin-top:0;
+    margin-bottom:12px;
+    color:cornflowerblue;
+    cursor:pointer;
+}
+#bcf_allclear:hover{
+        color:blue;
+}
 .bcf-wrap{
     margin-bottom: 12px;
+}
+.bcf_wrap_title{
+    font-size:20px;
+    font-weight: bold;
+}
+.bcf_wrap_clear{
+    float:right;
+    font-size:14px;
+    margin-top:6px;
+    color:cornflowerblue;
+    cursor:pointer;
+}
+.bcf_wrap_clear:hover{
+        color:blue;
 }
 .bcf_btn_wrap{
     display:flex;
     flex-wrap: wrap;
-    gap:12px;
+    gap:24px 12px;
+    margin-bottom: 24px;
+    margin-top:8px;
 }
 .checkBtn{
     input{
--- src/components/boothCard_container.vue
+++ src/components/boothCard_container.vue
@@ -1,24 +1,26 @@
 <template>
-    <div id="bcc_container">
-        <BoothCard v-for="(boothData , card_id) in filteringBoothDatas" :key="card_id"
-            :id="card_id"
-            :title="boothData.title"
-            :group="boothData.group"
-            :place="boothData.place"
-            :tags=boothData.tags
-            :describe="boothData.describe"
-            :imgsrc="boothData.imgsrc"
-            :date="boothData.date"
-        />
-        <div id="bcc_notfound" v-if="notfound"></div>
+    <div class="bcc_all">
+        <BoothCardFilterPC 
+            @sendChangedate="changeDate" 
+            @sendChangeplace="changePlace" 
+            @sendChangetag="changeTag" 
+            @sendWrapclear="wrapClear" 
+            @sendAllclear="allClear"
+            />
+        <div id="bcc_container">
+            <BoothCard v-for="(boothData , card_id) in filteringBoothDatas" :key="card_id"
+                :id="card_id"
+                :title="boothData.title"
+                :group="boothData.group"
+                :place="boothData.place"
+                :tags=boothData.tags
+                :describe="boothData.describe"
+                :imgsrc="boothData.imgsrc"
+                :date="boothData.date"
+            />
+        </div>
+        <div id="bcc_notfound" v-if="notfound">NOT FOUND<br /><br />お探しの展示は見つかりませんでした。<br />別の条件で検索してみてください。</div>
     </div>
-   <BoothCardFilterPC 
-        @sendChangeDate="changeDate" 
-        @sendChangePlace="changePlace" 
-        @sendChangeTag="changeTag" 
-        @allclear="allClear" 
-        @wrapclear="wrapClear" 
-        />
 </template>
 <!--コンポーネント読み込み用-->
 <script lang="ts">
@@ -55,21 +57,22 @@ export default {
                 this.selectedTags=[];
             }
         },
-        changeDate(date:String){
+        changeDate(date:string){
             if(this.selectedDates.includes(date)){
                 this.selectedDates.splice(this.selectedDates.indexOf(date),1);
             }else{
                 this.selectedDates.push(date);
             }
         },
-        changePlace(place:String){
+        changePlace(place:string){
             if(this.selectedPlaces.includes(place)){
                 this.selectedPlaces.splice(this.selectedPlaces.indexOf(place),1);
             }else{
                 this.selectedPlaces.push(place);
             }
         },
-        changeTag(tag:String){
+        changeTag(tag:string){
+            console.log(this.selectedTags);
             if(this.selectedTags.includes(tag)){
                 this.selectedTags.splice(this.selectedTags.indexOf(tag),1);
             }else{
@@ -81,7 +84,7 @@ export default {
         filteringBoothDatas(){
             let filtered:any;
             filtered=this.boothDatas;
-            if(this.selectedDates.length>0){
+            if(this.selectedDates.length>0 && this.selectedDates.length<2){
                 filtered=filtered.filter((item:any)=>{
                     return this.selectedDates.includes(item.date);
                 });
@@ -92,7 +95,7 @@ export default {
                 });
             }
             if(this.selectedTags.length>0){
-                filtered=filtered.filter((item:any)=>{
+                filtered=filtered.filter((item:any)=>{                    
                     for(let tag of item.tags){
                         if(this.selectedTags.includes(tag))return true;
                     }
@@ -103,7 +106,7 @@ export default {
         }
     },
     watch:{
-        boothDatas(newList){
+        filteringBoothDatas(newList){
             this.notfound=newList.length===0;
         }
     }
@@ -111,10 +114,21 @@ export default {
 </script>
 <style>
 #bcc_container{
-    margin-left:calc(mod(100% , (320px + 16px)) / 2 - 8px);
+    margin-left:calc(mod(75dvw , (320px + 16px)) / 2 - 8px + 25dvw);
     display:flex;
     flex-wrap: wrap;
     row-gap: 16px;
     gap:24px;
+    width:auto;
+}
+#bcc_all{
+    width:100%;
+    height:100%;
+}
+#bcc_notfound{
+    width: 75%;
+    margin-left:25%;
+    margin-top:10em;
+    text-align:center;
 }
 </style>
